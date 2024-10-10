73e382e5028f5b41f2c95bf4f77cb6cda1d8656d
Dineshtarun g <guymasked03@gmail.com>
2024-10-10 23:38:56 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,27 @@
+#day 3A about input and output and string concatination
+
+
+print ("input and output class day 3 ") #this is output 
+
+boy_name = input("ENTER THE BOY NAME : ")
+boy_age = int(input ("ENTER THE BOY AGE : "))
+giri_name = input("ENTER THE GIRL NAME : ")
+girl_age = int(input ("ENTER THE GIRL AGE : "))
+#TO FIND THE DIFFERENCE BETWEEN THESE TWO
+#age_diff(boy_age - girl_age)
+#to avoid error use particular data tye
+age_diff =  abs(int(boy_age - girl_age))
+#to print value without nagative use abs nga
+print (boy_name,giri_name)
+print (boy_name +(" loves ")+ giri_name)
+print ("age differnence between these two")
+print (age_diff)
+
+#formatted concatenation
+print (f" {boy_name } loves {giri_name} age diff is {str(age_diff)}")
+
+''' multi line comments 
+dbfbkdbfksdfb
+sbdfkjsjhlfsd
+'''
+print ("yooo")
