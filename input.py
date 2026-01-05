ad18fd27ee6c6dc3b39b82f035465fbae45a984d
tarun08-code <guymasked03@gmail.com>
2026-01-05 19:55:42 +0530

    din

---
+++ input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+print(name)
+
+#type(name)  # This will show that the type is str
+print(type(name))
---
+++ numbers.py
@@ -0,0 +1,12 @@
+print (8 + 2 )
+print (8 - 2 )
+print (8 * 2 )  
+print (8 / 2 )
+print (8 // 2 )
+print (8 % 2 )
+print (8 ** 2 ) #power 
+
+# working with numbers
+print (round (8.6))
+print (abs (-8.6))
+print (pow (8,2))
